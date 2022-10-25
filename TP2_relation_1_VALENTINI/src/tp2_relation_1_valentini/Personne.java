3f588549fb85f0958d063e09f7043ca986d328fb
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-10-25 12:55:56 +0200

    on commence la troisieme partie

---
+++ TP2_relation_1_VALENTINI/src/tp2_relation_1_valentini/Personne.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_valentini;
+
+/**
+ *
+ * @author arthu
+ */
+public class Personne {
+String Nom;
+String Prenom;
+
+public Personne (String nom, String prenom)
+        Nom= nom
+        Prenom=prenom
+}
