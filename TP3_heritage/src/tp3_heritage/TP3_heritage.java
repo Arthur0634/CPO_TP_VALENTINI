32019110efe125e6b397fbe49356c2493c0792e5
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-11-15 12:53:27 +0100

    fin partie 2

--- TP3_heritage/src/tp3_heritage/TP3_heritage.java
+++ TP3_heritage/src/tp3_heritage/TP3_heritage.java
@@ -4,7 +4,13 @@
  */
 package tp3_heritage;
 
+import tp3_heritage.armes.epée;
+import tp3_heritage.armes.Baton;
+import tp3_heritage.armes.Armes;
 import java.util.ArrayList;
+import tp3_heritage.personnage.Guerrier;
+import tp3_heritage.personnage.magicien;
+import tp3_heritage.personnage.personnage;
 
 /**
  *
@@ -16,15 +22,29 @@ public class TP3_heritage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        epée pago=new epée("Excalibur",7,5);
-        epée paga=new epée("Durandal",4,7);
-        Baton benoi=new Baton("Chêne",4,5);
-        Baton nafel=new Baton("Charme", 5, 6);
+        epée Excalibur=new epée("Excalibur",7,5);
+        epée Durandal=new epée("Durandal",4,7);
+        Baton Chêne=new Baton("Chêne",4,5);
+        Baton Charme=new Baton("Charme", 5, 6);
         ArrayList<Armes> tab= new ArrayList<Armes>();
-        tab.add("pago");
-        tab.add("paga");
-        tab.add("benoi");
-        tab.add("nafel");
+        tab.add(Excalibur);
+        tab.add(Durandal);
+        tab.add(Chêne);
+        tab.add(Charme);
+        
+        magicien Gandalf=new magicien("Gandalf",65,true);
+        magicien Garcimore=new magicien("Garcimore",44,false);
+        Guerrier Conan=new Guerrier("Conan", 78,false);
+        Guerrier Lannister=new Guerrier("Lannister", 45, true);
+        ArrayList<personnage> tab= new ArrayList<personnage>();
+        tab.add(Gandalf);
+        tab.add(Garcimore);
+        tab.add(Conan);
+        tab.add(Lannister);
+        
+        
+        
+                
     
         // TODO code application logic here
     }
