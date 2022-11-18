35c94f55361afb7c5e0fb042a514a40bef0dee2d
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-11-18 11:35:55 +0100

    Creation de la classe Jeton Fin.

---
+++ SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/super_puissance_4_moussally_valentini/SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package super_puissance_4_moussally_valentini;
+import Classes1.Jeton;
+import Classes1.*;
+/**
+ *
+ * @author arthu
+ */
+public class SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Jeton Arthur = new Jeton("Jaune");
+        // TODO code application logic here
+    }
+    
+}
