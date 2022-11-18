f29bc965bd47688a937cb9b507d64a4da14fb6c2
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-11-18 12:23:58 +0100

    creation de la classe cellule de grille

--- SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/Classes1/CelluleDeGrille.java
+++ SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/Classes1/CelluleDeGrille.java
@@ -10,5 +10,33 @@ package Classes1;
  * @author arthu
  */
 public class CelluleDeGrille {
+    Jeton jetonCourant;
+    private boolean avoirTrouNoir;  //on creer nos variables
+    private boolean avoirDesintegrateur;
     
+public CelluleDeGrille ()   {   //on affecter des valeurs de bases
+        jetonCourant = null;
+        avoirTrouNoir= false;
+        avoirDesintegrateur=false;
+    }
+public boolean presenceJeton () {  //permet de savoir si le jeton est present ou non
+    if (jetonCourant==null) {
+        return false;
+    }
+    else {
+        return true;
+    }
+                  
 }
+
+public void affecterJeton(Jeton jeton){ 
+    this.jetonCourant=jeton;
+
+}
+
+
+
+}
+    
+            
+
--- SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/SP4_console_MOUSSALLY_VALENTINI/SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI.java
+++ SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/SP4_console_MOUSSALLY_VALENTINI/SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package super_puissance_4_moussally_valentini;
+package SP4_console_MOUSSALLY_VALENTINI;
 import Classes1.Jeton;
 import Classes1.*;
 /**
@@ -15,7 +15,7 @@ public class SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Jeton Arthur = new Jeton("Jaune");
+        Jeton Arthur = new Jeton("Jaune"); //on test que le jeton prend bien la bonne couleur et retourne la bonne lettre
         // TODO code application logic here
         System.out.println(Arthur);
     }
