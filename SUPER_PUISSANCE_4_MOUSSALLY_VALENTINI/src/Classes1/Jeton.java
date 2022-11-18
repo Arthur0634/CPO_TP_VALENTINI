e485321e839b3785db4227febb6facf84e7fbcc9
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-11-18 11:35:34 +0100

    Creation de la classe Jeton Fin.

---
+++ SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/Classes1/Jeton.java
@@ -0,0 +1,33 @@
+package Classes1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arthu
+ */
+public class Jeton {
+    private String Couleur;
+    
+    public Jeton (String couleur) {
+        this.Couleur= Couleur;
+    }
+    
+    public String lireCouleur() {
+        return Couleur;
+    }
+   @Override
+public String toString() {
+    String C;
+    if (Couleur=="rouge"){
+        return "R";
+        
+    }
+    else{
+        return "J";
+    }
+}
+}
