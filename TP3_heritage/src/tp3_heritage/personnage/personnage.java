a490293ab4134dbcc739382d2d7ac655b537dec1
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-11-18 09:50:16 +0100

    fin partie 2

--- TP3_heritage/src/tp3_heritage/personnage/personnage.java
+++ TP3_heritage/src/tp3_heritage/personnage/personnage.java
@@ -9,30 +9,43 @@ package tp3_heritage.personnage;
  * @author arthu
  */
 public class personnage {
-String  nom;
-int niveau_de_vie;
-
-public int getniveau_de_vie(){
-    return niveau_de_vie;
-}
-
-public void setniveau_de_vie(int niveau_de_vie){
-    this.niveau_de_vie= niveau_de_vie;
-}
-
-public personnage(String unNom, int niveauvie){
-    nom=unNom;
-    niveau_de_vie=niveauvie;
-}
-
-public String tostring(){
-    String chaine_a_retourner;
-    chaine_a_retourner= nom+" ("+niveau_de_vie+"points de vie)";
-    return chaine_a_retourner;
-}   
-}
-
+    String Nomperso;
+    int Vie;
+    ArrayList<Armes> inventaire = new ArrayList<Armes>();
+    Armes arme_en_main;
     
-
-
-
+    public Personnage(String unNomPerso, int uneVie){
+        NomPerso=unNomPerso;
+        Vie=uneVie;
+        arme_en_main = null;
+    }
+    
+    public void ajouterArme (Armes arme_a_ajouter){
+        if (inventaire.size()<5){
+            inventaire.add(arme_a_ajouter);           
+        }
+    }
+    
+    public Armes getArme_en_main(){
+        return arme_en_main;
+    }
+    
+    public void EquipArme(String uneArme){
+        for (int i=0; i<inventaire.size();i++){
+            if (inventaire.get(i).getNom().equals(uneArme)){
+                arme_en_main=inventaire.get(i);
+                System.out.println("L'arme a bien ete affecte");
+            }else{
+                System.out.println("L'arme n'a pas ete trouvé");
+            }
+        }
+    }
+    
+    @Override
+    public String toString(){
+        String chaine;
+        chaine = "{Personnage: "+Nomperso+" Niveau de vie: "+Vie+"}";
+        return chaine;
+    }
+    
+}
