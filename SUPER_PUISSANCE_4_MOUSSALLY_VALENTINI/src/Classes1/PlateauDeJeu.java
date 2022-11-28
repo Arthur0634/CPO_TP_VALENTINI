29c6d212fc0d82f090ca0d28a8e7183ed294a1f3
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-11-28 12:18:36 +0100

    fin cellule de grille commencement plateau de jeu avec creation de tableau et de la fonction ajouterjetondanscolonne

--- SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/Classes1/PlateauDeJeu.java
+++ SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/Classes1/PlateauDeJeu.java
@@ -11,4 +11,22 @@ package Classes1;
  */
 public class PlateauDeJeu {
     
+CelluleDeGrille[][] grille = new CelluleDeGrille [6][7];//on initialise un tebleau de 42 cases qui represente le plateau de jeu
+  
+
+public  int ajouterJetonDansColonne(Jeton jeton, int colonne){
+        for (int ligne = 0;ligne<6;ligne++){
+            if (grille[ligne][colonne].presenceJeton()==false) {
+                grille[ligne][colonne].affecterJeton(jeton); //si la case du bas est libre, on y ajoute le jeton
+                return ligne; //puis on retourne l'indice de la ligne
+            }              
+        }
+        return -1; //en cas d’erreur
+    }
+
+public boolean grilleRemplie(){
+    
+    
+}
+
 }
