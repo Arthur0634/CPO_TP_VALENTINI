f19445672b7419e1b9a3da86013056b8401edd30
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-11-28 14:36:32 +0100

    fin de crreation fonction lignegagnantepourcouleur et colonnegagnantepourcouleur

--- SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/Classes1/PlateauDeJeu.java
+++ SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/Classes1/PlateauDeJeu.java
@@ -27,27 +27,44 @@ public  int ajouterJetonDansColonne(Jeton jeton, int colonne){
 public boolean grilleRemplie(){
     for (int i = 0;i<6;i++){//on parcours les lignes du tableau une par une
         for (int j = 0;j<7;j++){//on parcours les colonne du tableau une par une
-            if (grille[i][j].presenceJeton()==false){
-                    return false; 
+            if (grille[i][j].presenceJeton()==false){//on verifie si il y a un jeton sur  la case ij de la grille
+                    return false; //si il n'y a pas de jeton on retourne false
             }        
         }
     }
-    return true;
+    return true;//si il y a un jeton on retourne true
     
     
 }
 public String lireCouleurDuJeton(int x, int y){
-    return grille[x][y].lireCouleurDuJeton();
+    return grille[x][y].lireCouleurDuJeton();// cette fonction permet de lire et de retourner la couleur de la case ij
 }
 
-public boolean ligneGagnantePourCouleur(String){
-    for (int i = 0;i<3;i++){
-        for (int j = 0;j<7;j++){
-            if(lireCouleurDuJeton(i,j)==lireCouleurDuJeton(i+1,j)==.lireCouleurDuJeton(i+2,j)==.lireCouleurDuJeton(i+3,j)){
+public boolean ligneGagnantePourCouleur(String couleur){
+    for (int i = 0;i<6;i++){//on parcours les lignes du tableau une par une en allant jusqu'a la troisieme ligne car il faut un allignement de 4 jeton de meme couleur et apres la 3 case de la ligne ce n'est plus possible
+        for (int j = 0;j<4;j++){//on parcours les colonne du tableau une par une
+            if (grille[i][j].lireCouleurDuJeton().equals(couleur) && grille[i][j+1].lireCouleurDuJeton().equals(couleur) && grille[i][j+2].lireCouleurDuJeton().equals(couleur) && grille[i][j+3].lireCouleurDuJeton().equals(couleur)){ //on regarde si les 4 cases sont de meme couleur une a une sur la ligne
+                    return true; //on retourne true si elles sont toutes les 4 de la meme couleur
+            }
         }
     }
+             return false; //si on n'a trouve aucune case gagnante, on retourne false
    
 } 
+
+public boolean colonneGagnantePourCouleur(String couleur){
+         for (int i=0;i<3;i++){//on parcours les colonnes du tableau une par une en allant jusqu'a la quatrieme colonne car il faut un allignement de 4 jetons de meme couleur et apres la 4 case de la colonne ce n'est plus possible
+             for (int j=0;j<7;j++){//on parcours les lignes du tableau une par une
+                 
+                if (grille[j][i].lireCouleurDuJeton().equals(couleur) && grille[j][i+1].lireCouleurDuJeton().equals(couleur) && grille[j][i+2].lireCouleurDuJeton().equals(couleur) && grille[j][i+3].lireCouleurDuJeton().equals(couleur)){ //on verifie si les 4 aligne sur la meme colonne sont de meme couleur 
+                    return true; //on retourne true si les 4 cases aligne sont de meme couleur
+                }                                     
+                     
+             }
+         }
+         return false; //si on n'a trouve aucune case gagnante, on retourne false
+     }
+
 
 
 }
