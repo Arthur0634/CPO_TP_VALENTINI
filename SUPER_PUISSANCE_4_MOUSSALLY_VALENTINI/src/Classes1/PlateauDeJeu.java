d2538483501d5c7eb75477ec0627128d02c68b34
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-12-06 09:08:43 +0100

    mise en place des fonction de recuperation jeton desintegrateur et trou noir (pas encore dans la console )

--- SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/Classes1/PlateauDeJeu.java
+++ SUPER_PUISSANCE_4_MOUSSALLY_VALENTINI/src/Classes1/PlateauDeJeu.java
@@ -4,115 +4,153 @@ package Classes1;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author arthu
  */
 public class PlateauDeJeu {
-    
-CelluleDeGrille[][] grille = new CelluleDeGrille [6][7];//on initialise un tebleau de 42 cases qui represente le plateau de jeu
-  
 
-public  int ajouterJetonDansColonne(Jeton jeton, int colonne){
-        for (int ligne = 0;ligne<6;ligne++){
-            if (grille[ligne][colonne].presenceJeton()==false) {
+    CelluleDeGrille[][] grille = new CelluleDeGrille[6][7];//on initialise un tebleau de 42 cases qui represente le plateau de jeu
+
+    public PlateauDeJeu() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                grille[i][j] = new CelluleDeGrille(null);
+            }
+        }
+    }
+
+    public int ajouterJetonDansColonne(Jeton jeton, int colonne) {
+        for (int ligne = 5; ligne > -1; ligne--) {
+            if (grille[ligne][colonne].presenceJeton() == false) {
                 grille[ligne][colonne].affecterJeton(jeton); //si la case du bas est libre, on y ajoute le jeton
                 return ligne; //puis on retourne l'indice de la ligne
-            }              
+            }
         }
         return -1; //en cas d’erreur
     }
 
-public boolean grilleRemplie(){
-    for (int i = 0;i<6;i++){//on parcours les lignes du tableau une par une
-        for (int j = 0;j<7;j++){//on parcours les colonne du tableau une par une
-            if (grille[i][j].presenceJeton()==false){//on verifie si il y a un jeton sur  la case ij de la grille
-                    return false; //si il n'y a pas de jeton on retourne false
-            }        
-        }
-    }
-    return true;//si il y a un jeton on retourne true
-}
-
-public boolean colonneremplies(int a){//cette methode verifie si les colonne sont remplies pour ne pas epasser la quantite maximale de jetons par colonne
-    boolean res=true;
-    for(int i=0; i<6;i++){
-        if (grille[i][a-1].presenceJeton()==false){
-            res=false;
-            return res;
-        }
-    }
-    return res;
-}
-
-public String lireCouleurDuJeton(int x, int y){
-    return grille[x][y].lireCouleurDuJeton();// cette fonction permet de lire et de retourner la couleur de la case ij
-}
-
-public boolean ligneGagnantePourCouleur(String couleur){
-    for (int i = 0;i<6;i++){//on parcours les lignes du tableau une par une en allant jusqu'a la troisieme ligne car il faut un allignement de 4 jeton de meme couleur et apres la 3 case de la ligne ce n'est plus possible
-        for (int j = 0;j<4;j++){//on parcours les colonne du tableau une par une
-            if (grille[i][j].lireCouleurDuJeton().equals(couleur) && grille[i][j+1].lireCouleurDuJeton().equals(couleur) && grille[i][j+2].lireCouleurDuJeton().equals(couleur) && grille[i][j+3].lireCouleurDuJeton().equals(couleur)){ //on regarde si les 4 cases sont de meme couleur une a une sur la ligne
-                    return true; //on retourne true si elles sont toutes les 4 de la meme couleur
+    public boolean grilleRemplie() {
+        for (int i = 0; i < 6; i++) {//on parcour les lignes du tableau une par une
+            for (int j = 0; j < 7; j++) {//on parcour les colonnes du tableau une par une
+                if (grille[i][j].presenceJeton() == false) {//verifie si il y a un jeton dans la case ij sur la grille
+                    return false; //retourne false si il n'y a pas de jeton 
+                }
             }
         }
+        return true;//sinon on retourne true
     }
-             return false; //si on n'a trouve aucune case gagnante, on retourne false
-   
-} 
 
-public boolean colonneGagnantePourCouleur(String couleur){
-         for (int i=0;i<3;i++){//on parcours les colonnes du tableau une par une en allant jusqu'a la quatrieme colonne car il faut un allignement de 4 jetons de meme couleur et apres la 4 case de la colonne ce n'est plus possible
-             for (int j=0;j<7;j++){//on parcours les lignes du tableau une par une
-                 
-                if (grille[j][i].lireCouleurDuJeton().equals(couleur) && grille[j][i+1].lireCouleurDuJeton().equals(couleur) && grille[j][i+2].lireCouleurDuJeton().equals(couleur) && grille[j][i+3].lireCouleurDuJeton().equals(couleur)){ //on verifie si les 4 aligne sur la meme colonne sont de meme couleur 
-                    return true; //on retourne true si les 4 cases aligne sont de meme couleur
-                }                                     
-                     
-             }
-         }
-         return false; //si on n'a trouve aucune case gagnante, on retourne false
-     }
+    public boolean colonneremplies(int a) {//methode qui verifie si les colonne sont remplies de jetons 
+        boolean res = true;
+        if (grille[5][a].presenceJeton() == false) {
+            res = false;
+            return res;
+        }
+        return res;
+    }
 
-public boolean diagonaleMontanteGagnantePourCouleur(String couleur){
-         for (int i=0;i<3;i++){//on parcours les colonnes du tableau une par une en allant jusqu'a la quatrieme colonne car il faut un allignement de 4 jetons de meme couleur et apres la 4 case de la colonne ce n'est plus possible
-             for (int j=0;j<2;j++){//on parcours les lignes du tableau une par une sans depasser la troiseme car apres on ne peut pas avoir 4 cases alignées de meme couleur
-                 
-                if ( grille[j][i].lireCouleurDuJeton().equals(couleur) && grille[j+1][i+1].lireCouleurDuJeton().equals(couleur) && grille[j+2][i+2].lireCouleurDuJeton().equals(couleur) && grille[j+3][i+3].lireCouleurDuJeton().equals(couleur)){ //on verifie si les 4 aligne sur la meme colonne sont de meme couleur 
-                    return true; //on retourne true si les 4 cases aligne sont de meme couleur
-                }                                     
-                     
-             }
-         }
-         return false; //si on n'a trouve aucune case gagnante, on retourne false
-     }
+    public String lireCouleurDuJeton(int x, int y) {
+        return grille[x][y].lireCouleurDuJeton();// lit et retourne la couleur de la case ij
+    }
+    
+    
+    public boolean ligneGagnantePourCouleur(String couleur_gagnante) {
+        for (int i = 0; i < 6; i++) {//i est la ligne, j la colone
+            for (int j = 0; j < 4; j++) {
+                if (grille[i][j].lireCouleurDuJeton().equals(couleur_gagnante) & grille[i][j + 1].lireCouleurDuJeton().equals(couleur_gagnante) & grille[i][j + 2].lireCouleurDuJeton().equals(couleur_gagnante) & grille[i][j + 3].lireCouleurDuJeton().equals(couleur_gagnante)) {
+                    return (true);
+                }
+            }
 
-public boolean diagonaleDesencanteGagnantePourCouleur(String couleur){
-         for (int i=0;i<3;i++){//on parcours les colonnes du tableau une par une en allant jusqu'a la quatrieme colonne car il faut un allignement de 4 jetons de meme couleur et apres la 4 case de la colonne ce n'est plus possible
-             for (int j=0;j<3;j++){//on parcours les lignes du tableau une par une sans depasser la 4 car il ne êux pas ya voir un alignement de 4 jetons apres la quatrieme
-                 
-                if (grille[j][i].lireCouleurDuJeton().equals(couleur) && grille[j-11][i+1].lireCouleurDuJeton().equals(couleur) && grille[j-2][i+2].lireCouleurDuJeton().equals(couleur) && grille[j-3][i+3].lireCouleurDuJeton().equals(couleur)){ //on verifie si les 4 aligne sur la meme colonne sont de meme couleur 
-                    return true; //on retourne true si les 4 cases alignées sont de meme couleur
-                }                                     
-                     
-             }
-         }
-         return false; //si on n'a trouve aucune case gagnante, on retourne false
-     }
-public boolean etreGagnantePourCouleur(String a){
-    if(ligneGagnantePourCouleur("a")==true) {
-        return true;
+        }
+        return (false);
     }
-    if(colonneGagnantePourCouleur("a")==true) {
-        return true;
+
+    public boolean colonneGagnantePourCouleur(String couleur_gagnante) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (grille[i][j].lireCouleurDuJeton().equals(couleur_gagnante) & grille[i + 1][j].lireCouleurDuJeton().equals(couleur_gagnante) & grille[i + 2][j].lireCouleurDuJeton().equals(couleur_gagnante) & grille[i + 3][j].lireCouleurDuJeton().equals(couleur_gagnante)) {
+                    return (true);
+                }
+            }
+
+        }
+        return (false);
     }
-    if(diagonaleMontanteGagnantePourCouleur("a")==true) {
-        return true;
+
+    public boolean diagonaleMontanteGagnantePourCouleur(String couleur_gagnante) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (grille[i][j].lireCouleurDuJeton().equals(couleur_gagnante)
+                        & grille[i + 1][j + 1].lireCouleurDuJeton().equals(couleur_gagnante)
+                        & grille[i + 2][j + 2].lireCouleurDuJeton().equals(couleur_gagnante)
+                        & grille[i + 3][j + 3].lireCouleurDuJeton().equals(couleur_gagnante)) {
+                    return (true);
+                }
+            }
+
+        }
+        return (false);
     }
-    if(diagonaleDesencanteGagnantePourCouleur("a")==true) {
-        return true;
+
+    public boolean diagonaleDescendanteGagnantePourCouleur(String couleur_gagnante) {
+        for (int i = 3; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (grille[i][j].lireCouleurDuJeton().equals(couleur_gagnante) & grille[i - 1][j + 1].lireCouleurDuJeton().equals(couleur_gagnante) & grille[i - 2][j + 2].lireCouleurDuJeton().equals(couleur_gagnante) & grille[i - 3][j + 3].lireCouleurDuJeton().equals(couleur_gagnante)) {
+                    return (true);
+                }
+            }
+
+        }
+        return (false);
     }
-    return false;
+
+    public boolean etreGagnantePourCouleur(String couleur_gagnante) {
+        if (diagonaleDescendanteGagnantePourCouleur(couleur_gagnante) == true | diagonaleMontanteGagnantePourCouleur(couleur_gagnante) == true | colonneGagnantePourCouleur(couleur_gagnante) == true | ligneGagnantePourCouleur(couleur_gagnante) == true) {
+            return (true);
+        }
+        return (false);
+    }
+
+    public void afficherGrilleSurConsole() {
+        String afficher_ligne = "";
+        for (int i = 0; i < 6; i++) {
+            afficher_ligne = "";
+            for (int j = 0; j < 7; j++) {
+                afficher_ligne += "[" + grille[i][j].toString() + "]";
+            }
+            System.out.println(afficher_ligne);
+        }
+    }
+
+    public boolean presenceJeton(int x, int y) {
+        return (grille[x][y].presenceJeton());
+    }
+    
+    public boolean presenceTrouNoir(int x,int y) {
+        return (grille[x][y].presenceTrouNoir());
+    }
+    public void placerTrouNoir(int x,int y) {
+        grille[x][y].presenceTrouNoir();
+    }
+    public void supprimerTrouNoir(int x,int y) {
+        grille[x][y].supprimerTrouNoir();
+    }
+    public void placerDesintegrateur(int x,int y) {
+        grille[x][y].presenceDesintegrateur();
+    }
+    public void supprimerDesintegrateur(int x,int y) {
+        grille[x][y].supprimerDesintegrateur();
+    }
+    public boolean presenceDesintegrateur(int x,int y) {
+        return(grille[x][y].presenceDesintegrateur());
+    }
+    public void supprimerJeton(int x,int y) {
+        grille[x][y].supprimerJeton();
+    }
+    public Jeton recupererJeton(int x, int y) {
+        return grille[x][y].recupererJeton();
+    }
 }
-}
+
