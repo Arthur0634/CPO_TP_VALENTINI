9b0c2a357ce78daffce4806e2d16ecdb5bb1c863
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-12-09 11:23:23 +0100

    Debut de la creation du jeu on a creer les 4 classes , et on commencer a les completer

---
+++ Light_Off_Moussally_Valentini/src/light_off_moussally_valentini/Partie.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package light_off_moussally_valentini;
+import java.util.Scanner;
+
+/**
+ *
+ * @author arthu
+ */
+public class Partie {
+    
+    String nom;
+    private Joueur joueurAct = new Joueur (nom);
+    private Grille Plateau=new Grille();//on crée les deux attributs joueur et grille qui vont representer le joueur defini par le nom qu'il a rentré et sa grille de jeu
+    
+}
