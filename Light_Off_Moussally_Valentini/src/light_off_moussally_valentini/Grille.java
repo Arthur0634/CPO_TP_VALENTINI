807ce8905f1030afe95b3ac90d947048fae49bc7
arthu <arthu@LAPTOP-DR0R6CDK.education.lakanal.epf.fr>
2022-12-25 22:15:11 +0100

    Ajout interface graphique plus correction quelque bug

--- Light_Off_Moussally_Valentini/src/light_off_moussally_valentini/Grille.java
+++ Light_Off_Moussally_Valentini/src/light_off_moussally_valentini/Grille.java
@@ -10,18 +10,51 @@ package light_off_moussally_valentini;
  */
 public class Grille {
 
-    CelluleLumineuse[][] Plateau = new CelluleLumineuse[5][5];//on crée un tablea de 5 lignes et 5 colonnes qui contient les 25 cellules lumineuses
+    CelluleLumineuse[][] Plateau ;//on crée un tablea de 5 lignes et 5 colonnes qui contient les 25 cellules lumineuses
     boolean verif = false;
+    int TailleGrille = 5;
+ 
+    public Grille(int TailleGrilleACreer)
+    {
+        TailleGrille = TailleGrilleACreer;
+        Plateau = new CelluleLumineuse[TailleGrille][TailleGrille];
+        for (int i = 0; i < TailleGrille; i++) { //on balaye chaque cellule du tableau 
+            for (int j = 0; j < TailleGrille; j++) {
+                Plateau[i][j] = new CelluleLumineuse();
+            }
+        }
+        
+    }
 
-    public boolean Grille_vide() { 
-        for (int i = 0; i < 5; i++) { //on balaye chaque cellule du tableau 
-            for (int j = 0; j < 5; j++) {
+    public boolean Grille_vide() {
+        for (int i = 0; i < TailleGrille; i++) { //on balaye chaque cellule du tableau 
+            for (int j = 0; j < TailleGrille; j++) {
                 if (Plateau[i][j].est_allume() == true) { //si une cellule est allumé , la variable verif va passer a true 
                     verif = true; //on se servira de se resultat plus tard , il faudra a chaque coup verifier si la grille est vide 
-                                  //cad verifier si verif est egale a false 
+                    //cad verifier si verif est egale a false 
                 }
             }
         }
         return verif != true; //si rien n'est allumé , alors tout est eteint 
     }
+    
+    public  CelluleLumineuse[][] getCellules(){
+        return Plateau;
+    }
+            
+
+    public void change(int i, int j) {//cette methode permet de d'allumer ou d'eteindre la case que l'on souhaite
+        if (i >= 0 && i < TailleGrille && j >= 0 && j < TailleGrille) {//on verifie que i et j corrspondent bien a une cellule du tableau 
+            Plateau[i][j].change();
+        }
+    }
+    public void clic (int i,int j){// cette methode permet de changer toute les cases autour de la case cliqué
+        change(i,j);
+        change(i+1,j);
+        change(i-1,j);
+        change(i,j+1);
+        change(i,j-1);
+    }
+    
+
 }
--- Light_Off_Moussally_Valentini/src/light_off_moussally_valentini/Partie.java
+++ Light_Off_Moussally_Valentini/src/light_off_moussally_valentini/Partie.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package light_off_moussally_valentini;
+
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -10,9 +12,36 @@ import java.util.Scanner;
  * @author arthu
  */
 public class Partie {
-    
+
     String nom;
-    private Joueur joueurAct = new Joueur (nom);
-    private Grille Plateau=new Grille();//on crée les deux attributs joueur et grille qui vont representer le joueur defini par le nom qu'il a rentré et sa grille de jeu
+    private Joueur joueurAct = new Joueur(nom);
+    private Grille Plateau ;//on crée les deux attributs joueur et grille qui vont representer le joueur defini par le nom qu'il a rentré et sa grille de jeu
     
+    public Grille getPlateau() 
+    {
+        return Plateau;
+    }
+    public void placerCase(int nbcase) {//on place des cases lumineuses aleatoirement sur la grille
+        double alea;
+        int ligne;
+        int colonne;
+        Plateau = new Grille(nbcase);
+        for (int i = 0; i < nbcase; i++) { //pour placer 5 case allumées en debut de partie
+            alea = Math.random() * (nbcase-1);//on genere un double aleatoire entre 0 et 4 qui va designer les 5 lignes de la grille
+            ligne = (int) alea;//on prend la parti entiere du double
+            alea = Math.random() * (nbcase-1) ;//on genere un double aleatoire entre 0 et 4 qui va designer les 5 colonnes de la grille
+            colonne = (int) alea;
+            Plateau.change(ligne,colonne);
+        }
+        
+    }
+
+
+    public void initialiserPartie(int nbcase) {
+        Plateau = new Grille(nbcase);
+        placerCase(nbcase);
+       
+
+
+    }
 }
